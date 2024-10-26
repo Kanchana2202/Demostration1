41ce73db63d5cdd2214627d21a3d9457eae3d5a6
balaj <balaj@BKK-Legion>
2024-10-26 09:32:11 -0500

    sample printline

--- src/firstprogram/myfirstlivepgm.java
+++ src/firstprogram/myfirstlivepgm.java
@@ -5,6 +5,7 @@ public class myfirstlivepgm {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("hy i am dipin");
+System.out.println("pulling my first instance");
 	}
 
 }
